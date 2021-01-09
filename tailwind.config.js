feafd3a44f6af25ce5e6c1d24032e2933db7333d
shinth73 <60809725+shinth73@users.noreply.github.com>
2021-01-09 21:43:51 +0900

    20210109_2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+/** @format */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["sans-serif", "Helvetica", "Arial"],
+    },
+    extend: {
+      colors: {
+        lime: colors.lime,
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
